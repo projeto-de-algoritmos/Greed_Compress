7b735dcdbc08358b395d375ee60e68366fdb2f28
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2022-03-06 18:07:21 -0300

    Update huffman.py

--- src/huffman.py
+++ src/huffman.py
@@ -52,7 +52,7 @@ class HuffmanCoding:
 
             heapq.heappush(self.heap, merged)
 
-    def make_codes_helper(self, root, no_atual):
+    def codificador_auxiliar(self, root, no_atual):
         if (root == None):
             return
 
@@ -61,13 +61,13 @@ class HuffmanCoding:
             self.reverse_mapping[no_atual] = root.char
             return
 
-        self.make_codes_helper(root.left, no_atual + "0")
-        self.make_codes_helper(root.right, no_atual + "1")
+        self.codificador_auxiliar(root.left, no_atual + "0")
+        self.codificador_auxiliar(root.right, no_atual + "1")
 
     def make_codes(self):
         root = heapq.heappop(self.heap)
         codigo_atual = ""
-        self.make_codes_helper(root, codigo_atual)
+        self.codificador_auxiliar(root, codigo_atual)
 
     def codificado(self, texto):
         encoded_text = ""
@@ -83,3 +83,83 @@ class HuffmanCoding:
         padded_info = "{0:08b}".format(extra_padding)
         encoded_text = padded_info + encoded_text
         return encoded_text
+
+    def get_byte_array(self, padded_encoded_text):
+        if (len(padded_encoded_text) % 8 != 0):
+            print("Texto codificado não organizado")
+            exit(0)
+
+        b = bytearray()
+        for i in range(0, len(padded_encoded_text), 8):
+            byte = padded_encoded_text[i:i + 8]
+            b.append(int(byte, 2))
+        return b
+
+    def compress(self):
+        nomearquivo, file_extension = os.path.splitext(self.path)
+        caminho_saida = nomearquivo + ".bin"
+
+        with open(self.path, 'r+') as file, open(caminho_saida, 'wb') as output:
+            texto = file.read()
+            texto = texto.rstrip()
+
+            frequencia = self.cria_frequencia(texto)
+            self.make_heap(frequencia)
+            self.merge_nodes()
+            self.make_codes()
+
+            encoded_text = self.codificado(texto)
+            padded_encoded_text = self.bloco_codificado(encoded_text)
+
+            b = self.get_byte_array(padded_encoded_text)
+            output.write(bytes(b))
+
+        print("Comprimido")
+        return caminho_saida
+
+    """ functions for decompression: """
+
+    def remove_padding(self, padded_encoded_text):
+        padded_info = padded_encoded_text[:8]
+        extra_padding = int(padded_info, 2)
+
+        padded_encoded_text = padded_encoded_text[8:]
+        encoded_text = padded_encoded_text[:-1 * extra_padding]
+
+        return encoded_text
+
+    def decodifica_texto(self, encoded_text):
+        codigo_atual = ""
+        texto_decodificado = ""
+
+        for bit in encoded_text:
+            codigo_atual += bit
+            if (codigo_atual in self.reverse_mapping):
+                caracter = self.reverse_mapping[codigo_atual]
+                texto_decodificado += caracter
+                codigo_atual = ""
+
+        return texto_decodificado
+
+    def decompress(self, input_path):
+        nomearquivo, file_extension = os.path.splitext(self.path)
+        caminho_saida = nomearquivo + "_decompressed" + ".txt"
+
+        with open(input_path, 'rb') as file, open(caminho_saida, 'w') as output:
+            bit_string = ""
+
+            byte = file.read(1)
+            while (len(byte) > 0):
+                byte = ord(byte)
+                bits = bin(byte)[2:].rjust(8, '0')
+                bit_string += bits
+                byte = file.read(1)
+
+            encoded_text = self.remove_padding(bit_string)
+
+            decompressed_text = self.decodifica_texto(encoded_text)
+
+            output.write(decompressed_text)
+
+        print("Descomprimido")
+        return caminho_saida
