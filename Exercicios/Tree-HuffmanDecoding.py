f015c588c3c943b0dbcd253f84d4b75a329e86bf
castroricardo1 <ricardoloureiro75@gmail.com>
2022-03-07 09:17:15 -0300

    Exercicio Huffman Decoding

--- Exercicios/Tree-HuffmanDecoding.py
+++ Exercicios/Tree-HuffmanDecoding.py
@@ -1,3 +1,5 @@
+#https://www.hackerrank.com/challenges/tree-huffman-decoding/problem#:~:text=Huffman%20coding%20assigns%20variable%20length,character%20contain%20a%20code%20digit.
+
 #Complete the function decode_huff in the editor below. It must return the decoded string.
 
 #decode_huff has the following parameters:
